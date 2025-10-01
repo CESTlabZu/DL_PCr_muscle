8cc99acf065525caef474ca1b32b79a23ec2557f
Cestlab_Zu <126023075+CESTlabZu@users.noreply.github.com>
2025-09-30 19:00:51 -0500

    Update steadystatepulsesimgauss.m

--- fully_synthetic_simulations/steadystatepulsesimgauss.m
+++ fully_synthetic_simulations/steadystatepulsesimgauss.m
@@ -6,7 +6,6 @@ steadycheck = 100000;
 steadystate = 1;
 while (steadystate == 1 && t < time)
    blah = pulsesim1(dw, ksw1,ksw2, ksw3,ksw4,ksw5, kmw, mnots1,mnots2,mnots3, mnots4, mnots5, mnotw, mnotm, R1S, R2S1,R2S2, R2S3,R2S4,  R2S5, R1W, R2W, R1M, R2M, sep1,sep2,sep3,sep4, sep5, duration, curve, angle, init, TR, exciteflag, excitewait, exciteduration, exciteangle); 
-    %blah;
    p(t,:) = blah;
        if(spoil == 1)
            blah(4) = 0;
@@ -21,16 +20,7 @@ while (steadystate == 1 && t < time)
            blah(15) = 0;  
        end
    init = blah;
-  %t
    t = t+1;
-%%   if ( mod(t,4) == 0 )
-%%       if (floor(blah(6)*1000) == floor(steadycheck*1000 ))
-%%           steadystate = 0;
-           %t;
-%%       end
-%%       steadycheck = blah(6);
-%%   end
-   %p(t,:)= init;
 end
 p(t:time, :) = init;
 t;
